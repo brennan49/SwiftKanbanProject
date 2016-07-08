e6fd20aaf7db2f6311efdabee84a29dc193c0629
brennan49 <Brenndev_49>
2016-07-08 17:10:27 -0500

    2

--- soapTest.py
+++ soapTest.py
@@ -11,7 +11,6 @@ from email.mime.text import MIMEText
 #getAllUsersLastLogin(xs:string userLoginId, )
 
 threeMonths = date.today() + relativedelta(months=-3)
-print threeMonths
 
 url = 'https://login.swift-kanban.com/axis2/services/TeamMemberService?wsdl'
 client = Client(url)
@@ -50,34 +49,57 @@ def sortUsers(lastLoginInfo, currentDate):
     return usersToMessage
 
 theUsers = sortUsers(loginInfo, threeMonths)
-
+"""
 def sendMail(sender, userPass, toaddr):
     emailAddr = []
     for user in toaddr:
-        print user["emailAddress"]
         emailAddr.append(user["emailAddress"])
     msg = MIMEMultipart()
     msg['From'] = sender
-    msg['To'] = emailAddr
+    msg['To'] = ", ".join(emailAddr)
     msg['Subject'] = 'Automated message'
 
-    body = """This is an automated test message to multiple parties
-              If you get this, please let me know, I would like to test out
-
-              sending an e-mail with multiple lines and spaces.
-
-              Thank you,
-
-              Devlin"""
+    body = """"""
     msg.attach(MIMEText(body, 'plain'))
     #what are these values below for optum's servers?
     server = smtplib.SMTP('smtp.gmail.com', 587)
     server.starttls()
     server.login(sender, userPass)
     text = msg.as_string()
-    server.sendmail(msg["From"], msg["To"].split(","), text)
+    server.sendmail(msg.get("From"), emailAddr, text)
+    print "Message Sent"
     server.quit()
 
-sendees = [{"emailAddress": "devlin.brennan@optum.com"}, {"emailAddress": "tien.bui@optum.com"}]
+sendees = [{"emailAddress": "devlin.brennan@optum.com"}, {"emailAddress": "intreeged@gmail.com"}]
+
+sendMail("devlinbrennan@gmail.com", "Brenndev@49", sendees)"""
+
+def sendMessage(sender, userPass, toaddr):
+    msg = MIMEMultipart()
+    msg['From'] = sender
+    msg['To'] = toaddr
+    msg['Subject'] = 'Automated message'
+
+    body = """This is an automated test message to multiple parties.
+              If you get this, please let me know, I would like to test out
+
+              sending an e-mail with multiple lines and spaces.
+
+              Thank you,
+
+              Devlin
+              """
+    msg.attach(MIMEText(body, 'plain'))
+    #what are these values below for optum's servers?
+    server = smtplib.SMTP('smtp.gmail.com', 587)
+    server.starttls()
+    server.login(sender, userPass)
+    text = msg.as_string()
+    server.sendmail(sender, toaddr, text)
+    print "done"
+    server.close()
+
+sendees = ["devlinbrennan@gmail.com", "intreeged@gmail.com"]
 for sendee in sendees:
-    sendMail("devlinbrennan@gmail.com", "Brenndev@49", sendee)
+    sendMessage("devlinbrennan@gmail.com", "Brenndev@49", sendee)
+
