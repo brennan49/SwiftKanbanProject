15f84256c407596797359ad4e0f65bea37068629
brennan49 <Brenndev_49>
2016-07-11 11:12:38 -0500

    Done! Just need to check on specifications for the email to send

--- soapTest.py
+++ soapTest.py
@@ -8,31 +8,31 @@ import smtplib
 from email.mime.multipart import MIMEMultipart
 from email.mime.text import MIMEText
 
-#getAllUsersLastLogin(xs:string userLoginId, )
+#C:\Python27\python.exe C:\Users\dbrennan\PycharmProjects\SwitfKanban_Project\soapTest.py
 
-threeMonths = date.today() + relativedelta(months=-3)
+#Communicates with SwiftKanban servers to get a list of all users that have not logged
+#in in longer than 3 moths
+def getUsersToMessage():
+    url = 'https://login.swift-kanban.com/axis2/services/TeamMemberService?wsdl'
+    client = Client(url)
 
-url = 'https://login.swift-kanban.com/axis2/services/TeamMemberService?wsdl'
-client = Client(url)
+    security = Security()
+    token = UsernameToken('devlin.brennan@optum.com', 'devBrenn49')
+    security.tokens.append(token)
+    client.set_options(wsse=security)
 
-security = Security()
-token = UsernameToken('devlin.brennan@optum.com', 'devBrenn49')
-security.tokens.append(token)
-client.set_options(wsse=security)
+    result = client.service.getAllActiveUsersInOrg('devlin.brennan@optum.com')
+    listUsers = result[0]
+    users = []
+    for x in listUsers:
+        users.append(x["_loginId"])
+    lastLogin = client.service.getAllUsersLastLogin('devlin.brennan@optum.com', users)
+    loginInfo = lastLogin[0]
+    #pprint.pprint(loginInfo)
+    return loginInfo
 
-result = client.service.getAllActiveUsersInOrg('devlin.brennan@optum.com')
-listUsers = result[0]
-users = []
-for x in listUsers:
-    users.append(x["_loginId"])
-
-
-lastLogin = client.service.getAllUsersLastLogin('devlin.brennan@optum.com', users)
-
-loginInfo = lastLogin[0]
-#pprint.pprint(loginInfo)
-
-
+#Once list of users that haven't logged in in 3+ months, sort them into dictionaries
+#of just username and email.
 def sortUsers(lastLoginInfo, currentDate):
     usersToMessage = []
     for date in lastLoginInfo:
@@ -40,42 +40,57 @@ def sortUsers(lastLoginInfo, currentDate):
         lastLogin = theDate.split("T")
         currentDate = str(currentDate)
         lastLogin = str(lastLogin[0])
-        #print lastLogin
         threeMonths = datetime.strptime(currentDate, '%Y-%m-%d')
         lastLogged = datetime.strptime(lastLogin, '%Y-%m-%d')
         if(lastLogged < threeMonths):
             usersToMessage.append({"username": date["_userName"], "emailAddress": date["_emailAddress"]})
-
     return usersToMessage
 
-theUsers = sortUsers(loginInfo, threeMonths)
+#theUsers = sortUsers(loginInfo, threeMonths)
+#for users in theUsers:
+    #print users["emailAddress"]
 
-def sendMessage(sender, userPass, toaddr):
+def sendMail(sender, toaddr):
     msg = MIMEMultipart()
     msg['From'] = sender
     msg['To'] = toaddr
-    msg['Subject'] = 'Automated message'
+    msg['Subject'] = 'Automated message regarding your SwiftKanban Account'
 
-    body = """This is an automated test message to multiple parties.
-              If you get this, please let me know, I would like to test out
+    body = """Hello,
 
-              sending an e-mail with multiple lines and spaces.
+If you have recieved this message, it means that you have not logged into
+SwiftKanban in 3+ months.  As we would like to reattain these licenses for the
+software, we ask you that please either e-mail .... to have your account deleted
+if you no longer need it else, please log into your account.
 
-              Thank you,
+This message is set up to run once a month for people that have not logged in in over
+a month.
 
-              Devlin
-              """
+Thank you,
+
+..."""
     msg.attach(MIMEText(body, 'plain'))
     #what are these values below for optum's servers?
-    server = smtplib.SMTP('smtp.gmail.com', 587)
-    server.starttls()
-    server.login(sender, userPass)
+    server = smtplib.SMTP('mailo2.uhc.com', 25)
     text = msg.as_string()
     server.sendmail(sender, toaddr, text)
-    print "done"
-    server.close()
+    server.quit()
 
-sendees = ["devlinbrennan@gmail.com", "intreeged@gmail.com"]
-for sendee in sendees:
-    sendMessage("devlinbrennan@gmail.com", "Brenndev@49", sendee)
+#sendees = ["devlin.brennan@optum.com", "tien.bui@optum.com"]
+#for sendee in sendees:
+#    sendMail("devlin.brennan@optum.com", sendee)
 
+def main():
+   theUsers = getUsersToMessage()
+   threeMonths = date.today() + relativedelta(months=-3)
+   messageUsers = sortUsers(theUsers, threeMonths)
+   #pprint.pprint(messageUsers)
+   userEmails = []
+   for users in messageUsers:
+       userEmails.append(users["emailAddress"])
+   pprint.pprint(userEmails)
+   #sendees = ["devlin.brennan@optum.com", "tien.bui@optum.com"]
+   #for sendee in sendees:
+   #    sendMail("devlin.brennan@optum.com", sendee)
+
+main()
