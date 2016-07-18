93005b06a50076228b9920186418f0bcb92046a8
brennan49 <Brenndev_49>
2016-07-18 15:05:57 -0500

    finished v2

--- soapTest.py
+++ soapTest.py
@@ -15,12 +15,10 @@ from email.mime.text import MIMEText
 def getUsersToMessage():
     url = 'https://login.swift-kanban.com/axis2/services/TeamMemberService?wsdl'
     client = Client(url)
-
     security = Security()
-    token = UsernameToken('devlin.brennan@optum.com', 'devBrenn49')
+    token = UsernameToken('', '')
     security.tokens.append(token)
     client.set_options(wsse=security)
-
     result = client.service.getAllActiveUsersInOrg('devlin.brennan@optum.com')
     listUsers = result[0]
     users = []
@@ -28,7 +26,6 @@ def getUsersToMessage():
         users.append(x["_loginId"])
     lastLogin = client.service.getAllUsersLastLogin('devlin.brennan@optum.com', users)
     loginInfo = lastLogin[0]
-    #pprint.pprint(loginInfo)
     return loginInfo
 
 #Once list of users that haven't logged in in 3+ months, sort them into dictionaries
@@ -56,7 +53,7 @@ def sendMail(sender, toaddr):
 
 If you have recieved this message, it means that you have not logged into
 SwiftKanban in 3+ months.  As we would like to reattain these licenses for the
-software, we ask you that please either e-mail devlin.brennan@optum.com to have your account deleted
+software, we ask you that please either e-mail swift.users@optum.com to have your account deleted
 if you no longer need it else, please log into your account.
 
 This message is set up to run once a month for people that have not logged in in over
@@ -65,7 +62,6 @@ a month.
 Thank you
 """
     msg.attach(MIMEText(body, 'plain'))
-    #what are these values below for optum's servers?
     server = smtplib.SMTP('mailo2.uhc.com', 25)
     text = msg.as_string()
     server.sendmail(sender, toaddr, text)
@@ -75,13 +71,10 @@ def main():
    theUsers = getUsersToMessage()
    threeMonths = date.today() + relativedelta(months=-3)
    messageUsers = sortUsers(theUsers, threeMonths)
-   #pprint.pprint(messageUsers)
    userEmails = []
    for users in messageUsers:
        userEmails.append(users["emailAddress"])
-   pprint.pprint(userEmails)
-   sendees = ["devlin.brennan@optum.com", "tien.bui@optum.com", "michael_blaha@optum.com"]
-   for sendee in sendees:
-       sendMail("devlin.brennan@optum.com", sendee)
+   for sendee in userEmails:
+       sendMail("swift.users@optum.com", sendee)
 
 main()
